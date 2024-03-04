13d5e41ceaab051ec08baf56babc6f2085f55135
SRetip <shvayko.p.y@gmail.com>
2024-03-04 18:19:02 +0200

    !test

--- module/module1/tests/hello_test.rs
+++ module/module1/tests/hello_test.rs
@@ -7,5 +7,7 @@ fn example_test()
 {
 	assert_eq!( "hello world!".to_string(), hello() );
 	assert_eq!( "hello world!123".to_string(), hello() );
+	assert_eq!( "hello world!123".to_string(), hello() );
+
 	
 }
